a95981dc0c9e0097596dd67cb29efc329f3b86a0
naemcivic <naemcivic@gmail.com>
2016-03-28 14:38:15 -0400

    implement change to courses#new for Premium member access only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
 
   def index
     @user = User.all
-    @subscribers = Subscription.where('plan_id > ?', 1)
+    @subscribers = Subscription.where('plan_id >= ?', 1)
     @all_admins = User.where(admin: true)
   end
+
 end
